0337de2ce01606cecf895e6eed40f1769694818a
Paul Minton <paul.minton@calm.com>
2023-06-07 16:47:11 -0700

    fix default

--- aws_datalake/modules/emr/variables.tf
+++ aws_datalake/modules/emr/variables.tf
@@ -104,7 +104,7 @@ variable "task_instance_max_count" {
 variable "bootstrap_action" {
   description = "Ordered list of bootstrap actions that will be run before Hadoop is started on the cluster nodes"
   type        = map(string)
-  default     = []
+  default     = {}
 }
 
 locals {
