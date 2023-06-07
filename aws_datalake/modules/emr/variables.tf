4f2882b77c16bf878f2887a5f2226ba26599e4ea
Paul Minton <paul.minton@calm.com>
2023-06-07 16:13:00 -0700

    type is map of strings

--- aws_datalake/modules/emr/variables.tf
+++ aws_datalake/modules/emr/variables.tf
@@ -103,7 +103,7 @@ variable "task_instance_max_count" {
 
 variable "bootstrap_action" {
   description = "Ordered list of bootstrap actions that will be run before Hadoop is started on the cluster nodes"
-  type        = list(object)
+  type        = list(map(string))
 }
 
 locals {
